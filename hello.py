1dc96a0b00c2d722ff32ddf56f7f038b22e4c142
rg3915 <rg3915@yahoo.com.br>
2018-08-24 11:34:53 -0300

    view com template

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 
@@ -15,3 +16,8 @@ def view_da_rota1():
 @app.route('/rota2/<nome>')
 def view_da_rota2(nome):
     return 'Bem vindo, ' + nome + '! Essa é a view da /rota2!'
+
+
+@app.route("/ola/<nome>")
+def view_com_template(nome):
+    return render_template("index.html", nome=nome)
