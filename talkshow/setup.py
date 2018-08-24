6c4f4761956e76d1ad8e8656fddecc3c9c96a70a
rg3915 <rg3915@yahoo.com.br>
2018-08-24 14:35:05 -0300

    setup

---
+++ talkshow/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+requirements = [
+    'flask',
+    # 'pymongo',
+    # 'tinymongo',
+    # 'flask-simplelogin',
+    # 'flask-admin',
+    # 'flask-wtf',
+    # 'flask-restful',
+    # 'flask-pytest',
+    # 'flask-bootstrap',
+    'python-dotenv',
+    # 'flasgger'
+    'python-decouple',
+]
+
+setup(
+    name='talkshow',
+    version='0.0.1',
+    description="Call for papers system",
+    packages=['talkshow'],
+    include_package_data=True,
+    install_requires=requirements
+)
