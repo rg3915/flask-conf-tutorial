ab2e797b5f8ba11f8b32c4ba8a428ca0453a8670
rg3915 <rg3915@yahoo.com.br>
2018-08-24 17:44:45 -0300

    login_required no admin

--- talkshow/talkshow/ext/admin.py
+++ talkshow/talkshow/ext/admin.py
@@ -2,11 +2,11 @@ from flask_admin import Admin
 from flask_admin.base import AdminIndexView  # noqa
 from flask_admin.contrib.pymongo import ModelView  # noqa
 
-# from flask_simplelogin import login_required
+from flask_simplelogin import login_required
 
 # decorate Flask-Admin view via Monkey Patching
-# AdminIndexView._handle_view = login_required(AdminIndexView._handle_view)
-# ModelView._handle_view = login_required(ModelView._handle_view)
+AdminIndexView._handle_view = login_required(AdminIndexView._handle_view)
+ModelView._handle_view = login_required(ModelView._handle_view)
 
 
 def configure(app):
